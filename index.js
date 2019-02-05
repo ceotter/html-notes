f39921fb4ff1dadeeb5053a5269a4df1c2c6f918
Courtney Ott <courtneyott@Courtneys-MacBook-Pro-2.local>
2019-02-05 13:14:43 -0700

    Add a new to do

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
+document.addEventListener('DOMContentLoaded', onLoadContent)
+
 function onLoadContent(){
-  console.log("loaded")
+  //This button should get value from yay input
+  //and when pressed we should add a new li to 
+  //to our things list
+  console.log("hello")
+  document.getElementById ('yay-btn')
+    .addEventListener('click', function(){
+      document.getElementById('yay').value
+      console.log("New list item", newItem)
+    })
+  console.log( document
+    .getElementById('yay'))
+  document
+    .getElementById('yay')
+    .addEventListener('keyup', keyInputAddItem)
+  // Add a listener to the input element with id yay
+  // This listener hsould console.log("change")
+
 }
+function keyInputAddItem (event){
+  console.log("Keyup", this.value);
+}
+//Object Constructor
+
 function onClickYay(){
   console.log('clicked')
 }
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("loaded")
-})
- document
-  .getElementById('yay')
-  .addEventListener('click', function() {
-    console.log("clicked");
-  });
+
+
+let arr = ["Get groceries", 2, 6,"Drive", true]
+let numbers = []
+let content =[]
+for (var i = 0; i < arr.length; i++) {
+  if(typeof arr[i] === 'number') {
+    content.push(arr[i])
+  } else if (typeof arr[i] === 'number'){
+    numbers.push(arr[i])
+    }
+}
+console.log(content);
+
+var ulList = document.getElementById('things-list')
+
+for (var i = 0; i < content.length; i++) {
+  let listItem = document.createElement('li')
+  listItem.textContent = content[i]
+  ulList.append(listItem)
+}
+//document.getElementById('things-list')
